76536499c007821f7da2b360aa59c63e61992359
jihor <jihor@yandex.ru>
2017-05-16 21:39:54 +0300

    Loops

--- 01_python_language_basics/05_loops.py
+++ 01_python_language_basics/05_loops.py
@@ -32,6 +32,12 @@ print("\nIterate over keys with index")
 for number, k in enumerate(y):
     print("Key {} is {}".format(number, k))
 
+# Iterate over multiple iterables
+print("\nIterate over multiple arrays")
+# ends with the shortest iterable ending
+for a, b in zip(range(10, 60, 10), range(10)):
+    print(a + b)
+
 # Simple while loop
 print("\nSimple while loop")
 i = 0
@@ -49,3 +55,14 @@ for l in 'SomeNice624':
     if l.isnumeric():
         break
     print(l)
+
+# List comprehensions are special one-line-style loops
+print("\nList comprehensions")
+# ok up to 100, but will produce wrong results starting with 121, which is 11*11
+noprimes = [j for i in (2, 3, 5, 7) for j in range(i*2, 100, i)]
+primes = [x for x in range(2, 100) if x not in noprimes]
+print(primes)
+
+print([a for a in range(10) if a % 2 == 1])
+print(sorted(set([(a*b*c*d) for a in range(1, 4) for b in range(1, 4) for c in range(1, 4) for d in range(1, 4)])))
+
