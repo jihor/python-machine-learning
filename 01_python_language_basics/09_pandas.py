465f84aea06a5dea7607c5f4ba59d0462ac29487
jihor <jihor@yandex.ru>
2017-05-17 20:51:04 +0300

    Pandas

--- 01_python_language_basics/09_pandas.py
+++ 01_python_language_basics/09_pandas.py
@@ -1,5 +1,7 @@
 import pandas as pd, numpy as np, timeit as ti
 
+# Series
+print("Series")
 print(pd.Series(np.arange(101, 116)))       # series of int32's
 print(pd.Series((1, 2, 3)))                 # series of int64's
 print(pd.Series((1, "a", None)))            # series of objects
@@ -25,8 +27,8 @@ print(pd.Series(["lily", "scott", "mike"], index=[10, 20, 30]))   # can pass ind
 s4 = pd.Series({30: "scott", 5: "mike", 20: "lily", 7: "rael", 10: "roxy"}, index=[10, 20, 30])   # any values not in index wil be ignored
 print(s4)
 print(s4[30])            # scott - queried by index label
-print(s4.loc[30])        # scott - queried by index label
-print(s4.iloc[2])        # scott - queried by index position
+print(s4.loc[30])        # scott - queried by index label    (loc is not method, it's an attribute with __getitem__ and __setitem__ methods defined)
+print(s4.iloc[2])        # scott - queried by index position (same with iloc)
 
 simple_sum = """
 sum = np.int64(0)
@@ -57,3 +59,31 @@ s7 = s6.append(pd.Series(np.arange(2, 5)))
 print(s6)   # Still the same
 print(s7)   # Still the same
 print(s7.loc[2])        # now returns a row set, a new Series object
+# same with removing rows
+s7 = s6.append(pd.Series(np.arange(2, 5)))
+s8 = s7.drop(2)
+print(s8)        # now returns a row set, a new Series object
+print(s7)   # Still the same
+print(s8)
+s7.drop(2, inplace=True)
+print(s7)   # Changed
+
+# DataFrame is a two-axis Series
+print("\nDataFrames")
+person1 = {"name": "Dmitry", "age": 29, "sex": "male"}
+person2 = {"name": "Helga", "age": 30, "sex": "female"}
+person3 = pd.Series({"name": "Mike", "age": 31, "sex": "male"})
+person4 = pd.Series({"name": "Justine", "age": 26, "sex": "female"})
+df1 = pd.DataFrame([person1, person2, person3, person4])
+# indexes on both axis can be non-unique
+df2 = pd.DataFrame([person1, person2, person3, person4], index=["p1", "p1", "p2", "p2"])
+print(df2.loc["p1"])
+print("\n", df2.loc["p1", "name"])
+# chaining is relatively slow. It also returns copy of the data instead of the original.
+print("\n", df2.loc["p1"]["age"])
+# can't select column-wise directly, have to transpose the data frame. Use carefully.
+print("\n", df2.T.loc["name"])
+# slicing is also supported
+print("\n", df2.T.loc[:])
+print("\n", df2.loc["p1", ("age", "sex")])
+# append() and drop() work as in Series, creating copies by default
